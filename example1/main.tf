514b4a14b531712d7598387739ab3c9e117d3a4c
Titidevops <118744783+Titidevops@users.noreply.github.com>
2023-07-12 12:05:55 -0400

    Update main.tf

--- example1/main.tf
+++ example1/main.tf
@@ -1,1 +1,20 @@
-
+terraform {
+  required_version =  "~> 1.0"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+provider "aws" {
+  region = us-east-1
+  # Other AWS provider configuration settings
+}
+module "ec2module" {
+    source = "./ec2"
+    ec2name = "demo-ec2module"
+}
+output "module_output" {
+    value = module.ec2module.instance_id
+}
